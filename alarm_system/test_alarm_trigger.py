6f0fc88b0a7a6092eaa7aadba2c7bcf940a9db87
Mert Baykal <127455217+mertbaykal@users.noreply.github.com>
2025-02-17 12:40:59 +0300

    Delete alarm_system/test_alarm_trigger.py

--- alarm_system/test_alarm_trigger.py
+++
@@ -1,8 +0,0 @@
-# tests/test_alarm_trigger.py
-
-from src.agents.AlarmTrigger import AlarmTrigger
-
-def test_alarm_trigger():
-    trigger = AlarmTrigger()
-    assert trigger.trigger_alarm() == True
-
